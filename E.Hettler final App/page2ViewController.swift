ef996cc497e7a38e9a301aaeeaf34584c1b54b73
lowkey33 <85412876+lowkey33@users.noreply.github.com>
2021-07-31 13:31:20 -0500

    e.hettler

---
+++ E.Hettler final App/page2ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  page2ViewController.swift
+//  E.Hettler final App
+//
+//  Created by rvcstudent on 7/29/21.
+//
+
+import UIKit
+
+class page2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
